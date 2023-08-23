e4f680307a7ee37180db68703bb30fa336a4ddc4
Anton Krupin <krupin_anton@mail.ru>
2023-08-23 11:24:33 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import React from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Contacts from './routes/contacts';
+import StartPage from './routes/startPage';
+import UserAccount from './routes/userAccount';
+
+import routes from './routes/routes';
+
 import Header from './components/Header/Header';
-import Title from './components/Title/Title';
-import ServiceCards from './components/ServiceCards/ServiceCards';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="main">
-      <Header />
-      <Title />
-      <ServiceCards />
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path={routes.startPagePath()} element={<StartPage />} />
+      <Route path={routes.contactsPagePath()} element={<Contacts />} />
+      <Route path={routes.userAccountPath()} element={<UserAccount />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
