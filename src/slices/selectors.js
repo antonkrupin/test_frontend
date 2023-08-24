68a14bf46bba2e1dd75971f1b1fc2b4d68dcff2a
Anton Krupin <krupin_anton@mail.ru>
2023-08-24 14:17:31 +0300

    add selectors

--- src/slices/selectors.js
+++ src/slices/selectors.js
@@ -1,1 +1,5 @@
 export const fetchIsModalOpen = (state) => state.main.isModalOpen;
+export const fetchUsers = (state) => state.main.users;
+export const fetchAuthorizedUser = (state) => state.main.authorizedUser;
+export const fetchError = (state) => state.main.error;
+export const fetchIsAuthorized = (state) => state.main.isAuthorized;
