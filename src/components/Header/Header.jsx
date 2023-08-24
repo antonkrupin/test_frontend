a34057b4a90f268b729bf182bf8ca677c1f866a3
Anton Krupin <krupin_anton@mail.ru>
2023-08-24 14:18:45 +0300

    change Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchIsAuthorized } from '../../slices/selectors';
+
+import { setModalOpen, setIsAuthorized } from '../../slices/mainReducer';
 
 import i18next from '../../assets/i18next';
-
 import './Header.css';
 
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isAuthorized = useSelector(fetchIsAuthorized);
+
+  const enterButtonHandler = () => {
+    if (isAuthorized) {
+      dispatch(setIsAuthorized());
+      navigate('/');
+    } else {
+      dispatch(setModalOpen());
+    }
+  }
+
+  const openContactsPage = () => {
+    navigate('/contacts');
+  }
+
+  const btnText = isAuthorized ? i18next.t('Header.exitBtn') : i18next.t('Header.enterBtn');
+
   return (
     <div className="header">
       <div className="logo">
-        <img src="logo.png"
-          alt={i18next.t('Header.logoImgAlt')}
-          title={i18next.t('Header.logoImgAlt')}
-        />
+        <Link to="/">
+          <img src="logo.png"
+            alt={i18next.t('Header.logoImgAlt')}
+            title={i18next.t('Header.logoImgAlt')}
+          />
+        </Link>
       </div>
       <div className="assets">
-        <h4>{i18next.t('Header.contactsBtn')}</h4>
-        <button>{i18next.t('Header.enterBtn')}</button>
+        <button
+          id="headerContacts"
+          onClick={openContactsPage}
+        >
+          {i18next.t('Header.contactsBtn')}
+        </button>
+        <button
+          onClick={enterButtonHandler}
+        >
+          {btnText}
+        </button>
       </div>
     </div>
   )
