6da074f5b7efc18d0f27ea41d9f872b884787f37
Anton Krupin <krupin_anton@mail.ru>
2023-08-24 14:17:20 +0300

    add reducers

--- src/slices/mainReducer.js
+++ src/slices/mainReducer.js
@@ -4,6 +4,8 @@ const initialState = {
   isAuthorized: false,
   isModalOpen: false,
   error: null,
+  authorizedUser: [],
+  users: [],
 }
 
 const slice = createSlice({
@@ -13,11 +15,27 @@ const slice = createSlice({
 		setModalOpen: (state, action) => {
 			state.isModalOpen = !state.isModalOpen;
 		},
+    setUsers: (state, action) => {
+      state.users = action.payload;
+    },
+    setAuthorizedUser: (state, action) => {
+      state.authorizedUser = [...action.payload];
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+    setIsAuthorized: (state, action) => {
+      state.isAuthorized = !state.isAuthorized;
+    },
 	}
 });
 
 export const {
   setModalOpen,
+  setUsers,
+  setAuthorizedUser,
+  setError,
+  setIsAuthorized,
 } = slice.actions;
 
 export default slice.reducer;
