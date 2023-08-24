fa5af722a4a25985f8662b1bb20666faa7d1b4d6
Anton Krupin <krupin_anton@mail.ru>
2023-08-24 14:18:05 +0300

    change userAccount component

--- src/routes/userAccount.jsx
+++ src/routes/userAccount.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { fetchAuthorizedUser } from '../slices/selectors';
 
 import Button from '../components/Button/Button';
 
 const UserAccount = () => {
+  const userName = useSelector(fetchAuthorizedUser);
   return (
     <div className="title">
       <h1>
-        Привет, Сергей
+        Привет, {userName[0]}
       </h1>
       <div className="interactionButtons">
         <Button text="userAccountPage.exitBtn" styling="filled w228" id="enterBtn"/>
